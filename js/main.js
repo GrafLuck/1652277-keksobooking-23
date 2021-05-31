db51a461f1e039694c06bbd01b60579670e29df1
Lutsishin Dmitry <Lutsishin.Dmi@infotecs-nt>
2021-05-31 12:12:19 +0300

    Написал функцию для получения случайного целого числа из неотрицательного диапазона

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Основной код функции скопирован из https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  if (min < 0 || max < 0) {
+    return;
+  }
+  // Последняя строка функции обработала бы эту ситуацию правильно, но чтобы не выполнять много лишних действий сразу вернем результат
+  if (max === min) {
+    return min;
+  }
+
+  if (max < min) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+getRandomIntInclusive(10, -10);
